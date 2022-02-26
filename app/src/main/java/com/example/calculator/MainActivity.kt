beec6037a12cd278d4d31d2ad12422e257974391
MaryamTaheri <99006728+maryamtaheri88@users.noreply.github.com>
2022-02-26 16:41:04 +0330

    calculate

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -5,16 +5,21 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import com.example.calculator.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var binding: ActivityMainBinding
 
     private var canAddOperation = false
     private var canAddDecimal = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     fun allClearAction(view: View) {
@@ -55,92 +60,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculateResult(): String {
-
-        val digitsOperators = digitsOperation()
-        if (digitsOperators.isEmpty())  return ""
-
-        val timesDivision = timeDivisionCalculate(digitsOperators)
-        if (timesDivision.isEmpty())  return ""
-
-        val result = addSubtractCalculate(timesDivision)
-
-        return result.toString()
-    }
-
-    private fun addSubtractCalculate(passedList: MutableList<Any>): Float { // mohasebe jam va tafrigh
-        var result = passedList[0] as Float
-
-        for (i in passedList.indices){
-            if (passedList[i] is Char && i != passedList.lastIndex){
-                val operator = passedList[i]
-                val nextDigit = passedList[i+1] as Float
-                if(operator =='+')
-                    result +=nextDigit
-                if(operator =='-')
-                    result -=nextDigit
-            }
-        }
-        return result
-    }
-
-    private fun timeDivisionCalculate(passedList: MutableList<Any>): MutableList<Any> {  // mohasebe zard o taghsim
-        var list = passedList
-        while( list.contains('*')  || list.contains('/')){
-            list = callTimesDiv(list)
-        }
-        return  list
-    }
-
-    private fun callTimesDiv(passedList: MutableList<Any>): MutableList<Any> {
-            val newlist = mutableListOf<Any>()
-            var restartIndex = passedList.size
-
-            for (i in passedList.indices){
-                if( passedList[i] is Char && i != passedList.lastIndex && i < restartIndex){
-                   val operator = passedList[i]
-                   val prevDigit = passedList[i-1] as Float
-                   val nextDigit = passedList[i+1] as Float
-
-                    when(operator){
-                        '*' ->
-                        {
-                            newlist.add(prevDigit*nextDigit)
-                            restartIndex = i+1
-                        }
-
-                        '/' ->
-                        {
-                            newlist.add(prevDigit/nextDigit)
-                            restartIndex = i+1
-                        }
-                        else ->
-                        {
-                            newlist.add(prevDigit)
-                            newlist.add(operator)
-                        }
-                    }
-                }
-                if (i > restartIndex )
-                    newlist.add(passedList[i])
-            }
-            return newlist
-    }
-
-    private fun digitsOperation(): MutableList<Any> {  // reshtei az adad gerefte va dar yek list zakhire mikonad
-        val list = mutableListOf<Any>()
-        var currentDigit = ""
-        for (charecter in workingTV.text) {
-            if (charecter.isDigit() || charecter == '.')
-                currentDigit += charecter
-            else {
-                list.add(currentDigit.toFloat())
-                currentDigit = ""
-                list.add(charecter)
-            }
-        }
-        if (currentDigit != "")
-            list.add(currentDigit.toFloat())
-
-            return list
+        return ""
     }
 }
